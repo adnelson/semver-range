1da577c3d5648d52961e085d2901e464805e1dff
Allen Nelson <anelson@narrativescience.com>
2017-07-20 13:25:10 -0500

    "semver-range: bump version from 0.2.5 to 0.2.6"

--- project.nix
+++ project.nix
@@ -2,7 +2,7 @@
 , cabal-install, unordered-containers, QuickCheck, hspec }:
 mkDerivation {
   pname = "semver-range";
-  version = "0.2.5";
+  version = "0.2.6";
   src = ./.;
   isLibrary = true;
   buildDepends = [ base classy-prelude parsec text cabal-install
